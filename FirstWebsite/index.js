1f99d5af56433b1d2b65a1e95ec4304bf73b6e1d
Jamie Zhang <jamiezhang@Jamies-MacBook-Air.local>
2024-03-22 22:09:09 -0400

    init

---
+++ FirstWebsite/index.js
@@ -0,0 +1,12 @@
+// interactivity of the webpage -- actions
+
+// inspect browser -> outputs in website console
+console.log(`hewo owo`);
+console.log(`i wuv pizza!`);
+
+// window alert
+window.alert(`jamy.exe has stopped working`);
+window.alert(`also im very asian btw`);
+
+document.getElementById("myH1").textContent = `hello ppl and welcome to my first webpage!`;
+document.getElementById("myP").textContent = `ora ora ora`;
---
+++ LearnVariables/index.js
@@ -0,0 +1,23 @@
+// variable = a container that stores a value
+//                  behaves as if it were the value it contains 
+
+// 1. declaration  --->  let x;
+// 2. assignment  --->  x = 100;
+
+
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+let firstName = "jamy";
+
+console.log(age);
+console.log(price);
+console.log(gpa);
+
+console.log(`u are ${age} years old`);
+console.log(`the price is $${price}`);
+console.log(`ur gpa is: ${gpa}`);
+console.log(`gpa data type: `, typeof gpa);
+console.log(`ur name is ${firstName}`);
+console.log(`firstName data type: `, typeof firstName);
